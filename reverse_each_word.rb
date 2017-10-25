83c750b7d1982f3e1bed7ec1a8dd95283d132ea3
Chad Montoya <chadmontoya21@yahoo.com>
2017-10-25 03:22:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,14 @@
 
 def reverse_each_word(string)
-  rev = []
-  string.split.each { |word| rev << word.reverse}
-  rev.join(" ")
+  string.split.each { |word| word.reverse}.join(" ")
 end
 
 
 def reverse_each_word(string)
-  array = string.split.collect {|word| word.reverse}
-  array.join(" ")
+  string.split.collect {|word| word.reverse}.join(" ")
 end
+
+
+# def reverse_each_word(sentence)
+#  sentence.reverse.split.reverse.join(" ")
+# end
